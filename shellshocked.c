704e7c1ee13c3560260b44e0e921616465458067
TravisAdamson <tndadamson@gmail.com>
2023-07-26 07:46:00 -0700

    Added two macros

--- shellshocked.c
+++ shellshocked.c
@@ -1,1 +1,12 @@
 #include "shellshocked.h"
+
+/**
+ * main - Generates a simple shell
+ *
+ * Return: 
+ */
+int main(int argc, char *argv[])
+{
+
+}
+
--- shellshocked.h
+++ shellshocked.h
@@ -7,5 +7,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <time.h>
+#define BSIZE 1024
+#define prompt "Shellshocked $"
 
 #endif
