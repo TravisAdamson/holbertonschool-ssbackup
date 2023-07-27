f37d9cc88bab84dc6ba05b5779a651ea48296680
matternstsr <matternstsr@gmail.com>
2023-07-27 13:26:04 -0700

    added some print statements to see where im at and fixed the print_env needed char

--- print_env.c
+++ print_env.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "shellshocked.h"
 
 /*extern char **environ;*/
 
-int print_env(**environ)
+int print_envi(char **environ)
 {
-	printf("started  -- int print_env(**environ)");
     char **env = environ;
+    printf("started  -- int print_env(**environ)");
     while (*env != NULL) {
         printf("%s\n", *env);
         env++;
