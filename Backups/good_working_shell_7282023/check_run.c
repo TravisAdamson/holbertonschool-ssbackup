a6e58221ced90b0529c8012f0b9deb41da08ce35
matternstsr <matternstsr@gmail.com>
2023-07-28 11:26:49 -0700

    pushing after cleaning

---
+++ Backups/good_working_shell_7282023/check_run.c
@@ -0,0 +1,34 @@
+#include "shellshocked.h"
+extern char **environ;
+
+int check_run(char **argv, char *paths)
+{
+        pid_t pid = fork();
+        int status = 0;
+        int index = 2;
+
+	printf("PATH = %s\n", paths);
+
+	while (argv[index] != NULL)
+        {
+                argv[index][strcspn(argv[index], "\n")] = '\0';
+                index++;
+        }
+        if (pid == -1)
+        {
+                perror("fork");
+                exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+                execvp(argv[1], argv);
+                perror("execvp");
+                exit(EXIT_FAILURE);
+        }
+        else
+        {
+                waitpid(pid, &status, 0);
+        }
+        return (1);
+}
+
