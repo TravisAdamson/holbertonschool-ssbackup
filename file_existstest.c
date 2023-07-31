6f94498323aac5343ffa6743bb186ccf34b92377
TravisAdamson <tndadamson@gmail.com>
2023-07-31 08:22:42 -0700

    Trying new access path

--- file_existstest.c
+++ file_existstest.c
@@ -1,17 +1,19 @@
-#include <sys/stat.h>
+#include "shellshocked.h"
 
 int check_file_exists(char *command, char **tokens)
 {
         int exists, index;
         char *temp_command;
+	char *slash = "/";
         struct stat buffer;
 
         index = 0;
         exists = 1;
         while ((exists == 1) && (tokens[index] != NULL))
         {
-                temp_command = malloc(strlen(tokens[index]) + strlen(command));
+                temp_command = malloc(strlen(tokens[index]) + strlen(command) + 2);
                 strcpy(temp_command, tokens[index]);
+		strcat(temp_command, slash);
                 strcat(temp_command, command);
                 exists = stat(temp_command, &buffer);
                 free(temp_command);
