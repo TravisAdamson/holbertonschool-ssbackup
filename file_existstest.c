062e5a82ef435984084e2665c6c1e32984965f67
matternstsr <matternstsr@gmail.com>
2023-07-31 08:31:46 -0700

    sending compiled file_exists

--- file_existstest.c
+++ file_existstest.c
@@ -1,19 +1,19 @@
 #include "shellshocked.h"
 
+#include <sys/stat.h>
+
 int check_file_exists(char *command, char **tokens)
 {
         int exists, index;
         char *temp_command;
-	char *slash = "/";
         struct stat buffer;
 
         index = 0;
         exists = 1;
         while ((exists == 1) && (tokens[index] != NULL))
         {
-                temp_command = malloc(strlen(tokens[index]) + strlen(command) + 2);
+                temp_command = malloc(strlen(tokens[index]) + strlen(command));
                 strcpy(temp_command, tokens[index]);
-		strcat(temp_command, slash);
                 strcat(temp_command, command);
                 exists = stat(temp_command, &buffer);
                 free(temp_command);
