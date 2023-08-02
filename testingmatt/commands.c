1b9d1856a33b1ae1749a4f2abb7d9f707f93eff8
matternstsr <matternstsr@gmail.com>
2023-08-01 20:00:52 -0700

    updated testing betty compliance

--- testingmatt/commands.c
+++ testingmatt/commands.c
@@ -1,29 +1,37 @@
 #include "shellshocked.h"
 
-void run_pwd() {
-    char cwd[PATH_MAX];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("%s\n", cwd);
-    } else {
-        perror("pwd");
-    }
+void run_pwd()
+{
+	char cwd[PATH_MAX];
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+	{
+		printf("%s\n", cwd);
+	}
+	else
+	{
+		perror("pwd");
+	}
 }
-void run_echo(const char *args[]) {
-    int i;
-    for (i = 1; args[i] != NULL; i++) {
-        printf("%s ", args[i]);
-    }
-    printf("\n");
+void run_echo(const char *args[])
+{
+	int i;
+	for (i = 1; args[i] != NULL; i++)
+	{
+		printf("%s ", args[i]);
+	}
+	printf("\n");
 }
-void run_cd(const char *args[]) {
-    if (args[1]) {
-        if (chdir(args[1]) == -1) {
-            perror("cd");
-        }
-    } else {
-        fprintf(stderr, "cd: missing argument\n");
-    }
+void run_cd(const char *args[])
+{
+	if (args[1])
+	{
+		if (chdir(args[1]) == -1)
+		{
+			perror("cd");
+		}
+	}
+	else
+	{
+		fprintf(stderr, "cd: missing argument\n");
+	}
 }
-
-
-
--- testingmatt/env_check.c
+++ testingmatt/env_check.c
@@ -1,9 +1,11 @@
 #include "shellshocked.h"
 
-void enumerate_environment(char *envp[]) {
-    int i = 0;
-    while (envp[i] != NULL) {
-        printf("%s\n", envp[i]);
-        i++;
-    }
+void enumerate_environment(char *envp[])
+{
+	int i = 0;
+	while (envp[i] != NULL)
+	{
+		printf("%s\n", envp[i]);
+		i++;
+	}
 }
