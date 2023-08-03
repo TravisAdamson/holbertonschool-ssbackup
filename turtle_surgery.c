21e2019ec2897a7b3fbe964f4173dd9167748d33
TravisAdamson <tndadamson@gmail.com>
2023-08-03 07:57:38 -0700

    Testing

--- turtle_surgery.c
+++ turtle_surgery.c
@@ -11,24 +11,21 @@
 int turtle_surgery(char *input, char **cmdS, char *delim)
 {
 	int iter = 0;
+	int st = 0;
 	char *cmdT = NULL, *dup = NULL;
 
 	for (dup = input, iter = 0; (cmdT = shell_cracked(&dup, delim)); iter++)
 	{
 		cmdS[iter] = cmdT;
-		if ((strcmp("exit", cmdS[0]) == 0) && (iter != 0))
-		{
-			set_turtle_free_or_not(cmdS);
-			if (input)
-				free(input);
-			exit(2);
-		}
 		if (strcmp("exit", cmdS[0]) == 0)
 		{
 			set_turtle_free_or_not(cmdS);
 			if (input)
+			{
+				st = 2;
 				free(input);
-			exit(0);
+			}
+			exit(st);
 		}
 	}
 	if (cmdT != NULL)
